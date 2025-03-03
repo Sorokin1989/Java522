cb44cd7183c1d6e5cbcb8837414988269f92695c
Dmitrii <sorokindm1try@yandex.ru>
2025-03-03 16:06:54 +0300

    homeWork_1And_2

---
+++ src/homeWork_1/Test.java
@@ -0,0 +1,11 @@
+package homeWork_1;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("My name is Dima");
+        System.out.println("I`m 35 years old");
+        System.out.println("I`m from Cheboksary");
+        System.out.println("I`m working");
+        System.out.println("I`m studing to be a programmer");
+    }
+}
