2d2ade6df74b3c1a431f48561746f7f0e82e3243
Dmitrii <sorokindm1try@yandex.ru>
2025-03-07 15:13:38 +0300

    classWork_1

---
+++ src/classWork/Java522-main/Java/2 - Hello world/Hello/src/Farid.java
@@ -0,0 +1,295 @@
+import javax.xml.crypto.dom.DOMCryptoContext;
+
+public class Farid {
+    public static void main(String[] args) {
+
+        // JDK -> Java Development Kit (Набор разработчика Java)
+        //()
+        //{}
+        //[]
+
+
+
+
+        //  skcsok
+
+        /*
+        1
+        2
+        3
+        4
+        5
+        6
+         */
+
+
+
+        // sort
+        //code
+
+
+//        System.out.println("Farid kak dela?");
+
+
+//        System.out.println("Farid");//print + perexod na novuyu liniyu
+//        System.out.println("kak dela?");
+
+
+//        System.out.print("Farid");
+
+//        System.out.println("Farid");
+
+
+//        System.out.println(
+//                "Farid Abdullayev" +
+//                " Kak dela?");
+
+
+//        System.out.println("Privet\nk" +
+//                "ak\ndela\n!");
+
+        //+
+        //cкладывание  System.out.println(10+10) => 20
+        //конкатенация System.out.println("Farid" + "Farid") => FaridFarid
+
+
+//        System.out.println("Farid"+" "+"Abdullayev . Mne " + 29);//FaridAbdullayev
+
+
+//        System.out.println("""
+//               *******
+//               ****           ***
+//               ****                   ***
+//                """);
+//
+//
+//        System.out.println("***********                " +  "***********");
+
+
+//        System.out.println("****\n****\n****\n");
+
+//        System.out.println("*****");
+//        System.out.println("*****");
+//        System.out.println("*****");
+
+
+//        System.out.println("* * * * * *");
+//        System.out.println("*         *");
+//        System.out.println("*         *");
+//        System.out.println("* * * * * *");
+
+        ////////////////////////////////////////////////////////////////////////////////////////////
+
+        //Переменные
+
+//        System.out.println("Farid Abdullayev . Mne 29");
+
+
+        //int  -> 1,2,3,4,4000,-550
+
+        //type name;
+
+//        int a5a;
+
+        //Нельзя
+        //1) Ключевые слова использовать нельзя (if,static,void)
+        //2) Знаки пунктуации
+        //3) В начале нельзя использовать числа ( в середине в конце можно)
+
+
+//        int a;//obyevili peremennuyu
+//        a = 10;//inicializaciya
+
+//        int a ;
+//        a = 10;
+
+//        int a = "Farid";
+//        int a = 10;//
+//        System.out.println(a);
+
+
+//        int x;
+//        int X;
+
+
+//        int x,y,d,f,g,e,w,q;
+
+//        int x;
+//        int y;
+//        int f;
+//        int d;
+//        int g;
+
+//        int q,w,e,r,t,y;
+//        q = 10;
+//        w = 20;
+//        e = 30;
+//        r = 10;
+
+
+//        int q = 10 , w = 20 , c = 10,d=1;
+
+//        int q;
+
+
+//        int x;
+//        int y;
+
+
+//        int x;
+//        x = 10;
+//        System.out.println(x);//10
+//        x = 20;
+//        System.out.println(x);//20
+//        x = 30;
+//        System.out.println(x);//30
+
+//        int x = 10,y = 10,c = 10;
+//
+//        System.out.println(x);
+//        x = y = c = 10;//x = 10 ; y = 10 ; c = 10;
+
+//        int x = 10 ;
+//        int y = 20;
+//        System.out.println(x+y);
+
+        //Ошибки
+
+        //1) Ошибка компиляции   (горит крассным)
+        //2) Ошибка при выполнении (10/0)
+
+//        20:25
+
+
+
+//        int a = 143_123_256;
+//
+//        System.out.println(a);
+
+
+//        int x = 29;
+
+//        int age = 29;
+
+
+
+//        int result = 1000000000+ 1000000000+ 1000000000;
+//        System.out.println(result);
+
+
+//        int l = 11111111111111111111;
+
+
+//        int x , y;
+//        x = 10;
+
+
+//        int age = 29;
+//        int x = age;
+//        System.out.println(x);
+
+//        int x = 10,age = x;
+
+//        int x = age = 10;
+
+
+        // =    ->    присвоение   x = 10
+        // ==   ->    равенство    x == y
+
+
+//        int x = 10;
+
+//       int x = 10;
+
+
+        //Переменная - это область памяти в хоторой храниться значение ,
+        //которое в процессе можно использовать при вылонении программы
+
+
+//        System.out.println("Mne 30");
+//        System.out.println("Mne 30");
+//        System.out.println("Mne 30");
+//        System.out.println("Mne 30");
+//        System.out.println("Mne 30");
+//        System.out.println("Mne 30");
+//        System.out.println("Mne 30");
+
+//        int age = 30;
+//        System.out.println("Mne " + age);
+//        age = age + 1;
+//        System.out.println("Mne " + age);
+//        age = age + 1;
+//        System.out.println("Mne " + age);
+//        age = age * 2;
+//        System.out.println("Mne " + age);
+//        age = age + 1;
+//        System.out.println("Mne " + age);
+//        age = age + 1;
+//        System.out.println("Mne " + age);
+//        age = age + 1;
+//        System.out.println("Mne " + age);
+//        age = age + 1;
+//        System.out.println("Mne " + age);
+//
+//
+//        System.out.println("Mne 40");//Литерал
+        //Литерал - это простые значени которые напрямую записаны а коде ;
+
+        //1 byte = 8 bit
+
+        //Основные типы данных
+
+        //Целые числа (DEFAULT int)
+//        byte              -           1 byte   (-128 + 127)
+//        short             -           2 byte   (-32k + 32k)
+//        int               -           4 byte   (-2 147xxx xxx + 2 147xxx xxx)
+//        long              -           8 byte   (-xxxxxxxxxxxxxxxx +xxxxxxxxxxxxxxxxx) (L , l)
+
+        //Дробные числа (DEFAULT Double)
+//        float              -          4 byte   (-xxxxxxxxxxxxxxxx +xxxxxxxxxxxxxxxxx)  (f , F)
+//        double             -          8 byte   (-xxxxxxxxxxxxxxxx +xxxxxxxxxxxxxxxxx)
+
+
+//        double d = 10.456 ;
+//        float a = 10.5f;
+
+//        Long l =  3_000_000_000L;
+//        System.out.println(l);
+
+
+//
+//        int x = 10;
+//        float f = 10.5f;
+//        System.out.println(x+f);
+
+
+//        int a = 2147483645;
+
+//        System.out.println(a);
+//        a = a + 1;
+//        System.out.println(a);
+//        a = a + 1;
+//        System.out.println(a);
+//        a = a + 1;
+//        System.out.println(a);
+
+//        byte b = 127;
+//        System.out.println("b=" + b);
+//        short s = 20000;
+//        System.out.println("s=" + s);
+//        int i = 2_147_111_111;
+//        System.out.println("i=" + i);
+//        long l = 100;
+//        System.out.println("l=" + l);
+
+
+        //1.500.000
+
+        //29:59:59
+        //12
+
+
+        // 256gb , 512 , 1tb
+
+    }
+}
---
+++ src/classWork/Java522-main/Java/4 - If/if/src/Main.java
@@ -0,0 +1,543 @@
+public class Main {
+    public static void main(String[] args) {
+        // Условные конструкции   IF , SWITCH
+
+//        if (условие){
+//            //тело
+//        }
+
+//         int age = 18;
+//
+//        System.out.println(age>=18);
+//
+//        //true , false
+//         if (age>=18){
+//             System.out.println("Заходите");
+//         }
+
+
+//        System.out.println("Start");
+//
+//        if (false){
+//            System.out.println("IF");
+//        }
+//
+//        System.out.println("End");
+
+
+//        System.out.println("Start");
+//
+//        boolean isAdmin = true;
+//
+//
+//        // a>b a>=b a==b a!=b a == true
+//
+//        int a= 10;
+//
+//        if (a==0){
+//            System.out.print("ADMIN ");
+//        }
+//        System.out.println("Farid");
+//
+//        System.out.println("End");
+
+
+//        boolean isAdmin = true;
+//        boolean isUser = false;
+//
+//        if (isAdmin){
+//            System.out.print("ADMIN ");
+//        }
+//
+//        if (isUser){
+//            System.out.print("USER ");
+//        }
+//
+//        System.out.println("Farid");
+
+//        //////////////////////////////////////////////////////////////////////////////
+//
+//        boolean isAdmin = false;
+//
+//
+//        if (isAdmin==true){
+//            System.out.print("ADMIN ");
+//        }
+//
+//        if (isAdmin==false){
+//            System.out.print("USER ");
+//        }
+//
+//        System.out.println("Farid");
+
+//        //////////////////////////////////////////////////////////////////////////////
+//
+//        boolean isAdmin = true;
+//
+//
+//        if (isAdmin){
+//            System.out.print("ADMIN ");
+//        }
+//
+//        if (isAdmin==false){
+//            System.out.print("USER ");
+//        }
+//
+//        System.out.println("Farid");
+
+
+//        //////////////////////////////////////////////////////////////////////////////
+//
+////        System.out.println(true);
+////        System.out.println(!true);
+//        boolean isAdmin = true;
+//
+//        //== != true  false   !true
+//        if (isAdmin){
+//            System.out.print("ADMIN ");
+//        }
+//
+//        if (!isAdmin){
+//            System.out.print("USER ");
+//        }
+//
+//        System.out.println("Farid");
+
+
+        //////////////////////////////////////////////////////////////////////////////
+
+//        boolean isAdmin = true;
+//
+//        if (isAdmin){
+//            System.out.print("ADMIN ");
+//        }
+//
+//        if (!isAdmin){
+//            System.out.print("USER ");
+//        }
+//
+//        System.out.println("Farid");
+
+////////////////////////////////////////////////////////////////////
+
+//        int num = -10;
+//
+//        if (num > 0){//net
+//            System.out.print("Positiv ");
+//        }
+//
+//        if (num < 0){
+//            System.out.print("Neqativ ");
+//        }
+//
+//        System.out.println(num);
+
+
+        ///////////////////////////////////////////////////////////////////
+
+//        if (условие){
+//          если true
+//        }else {
+//            если false
+//        }
+
+//        if (true){
+//
+//        }
+
+//        if (true){
+//
+//        }else{
+//
+//        }
+
+//        int num = 0;
+//
+//        if (num < 0){
+//            System.out.print("Neqativ ");
+//        }else {
+//            System.out.print("Positiv ");
+//        }
+//
+//        System.out.println(num);
+
+
+//        boolean isAdmin = true;
+//
+//        if (isAdmin == false){
+//            System.out.print("USER ");
+//        }else {
+//            System.out.print("ADMIN ");
+//        }
+//
+//        System.out.println("Farid");
+
+
+        ///////////////////////////////////////////////////////////////
+
+
+//        System.out.println(10==10);//true
+//        System.out.println(10>10);//false
+//        System.out.println(10<10);//false
+//        System.out.println(10<=10);//true
+//        System.out.println(10!=10);//false
+//        System.out.println(true==true);//true
+//        System.out.println(false==false);//true
+//        System.out.println(true==false);//false
+//        System.out.println(true!=false);//true
+//        System.out.println(true!=true);//false
+
+//        boolean isAdmin = true;
+//
+//        if (isAdmin == false){
+//            System.out.print("USER ");
+//        }else {
+//            System.out.print("ADMIN ");
+//        }
+//
+//        System.out.println("Farid");
+
+
+        /////////////////////////////////////////////////////////////
+
+
+//        int num = 0;
+//        System.out.println("start");
+//        if (num>0){
+//            System.out.print("positive");
+//        }else if(num<0){
+//            System.out.print("neqativ");
+//        }else{
+//            System.out.println("znacit eto 0");
+//        }
+//
+//        System.out.println("end");
+
+
+//        if((10+10-20)==0){
+//
+//        }
+//
+//        if((10+10-20)==0){
+//
+//        }
+//
+//        if((10+10-20)==0){
+//
+//        }
+
+
+//        if (true){
+//            System.out.println("DA");
+//        }else{
+//            System.out.println("NET");
+//        }
+
+//        String vinovnik = "Druq";
+//
+//        if (vinovnik=="Brat"){
+//            System.out.println("Brat slomal");
+//        }else if(vinovnik=="Papa"){
+//            System.out.println("Papa slomal");
+//        }else if(vinovnik=="Druq"){
+//            System.out.println("Druq slomal");
+//        } else{
+//            System.out.println("Mama slomala");
+//        }
+
+
+//        int num = 10;
+//        if (num > 0){
+//            if (num > 0){
+//                if (num > 0){
+//                    if (num > 0){
+//
+//                    }else if (num<0){
+//
+//                    }else{
+//
+//                    }
+//                }else if (num<0){
+//                    if (num > 0){
+//                        if (num > 0){
+//
+//                        }else if (num<0){
+//                            if (num > 0){
+//
+//                            }else if (num<0){
+//
+//                            }else{
+//
+//                            }
+//                        }else{
+//                            if (num > 0){
+//
+//                            }else if (num<0){
+//
+//                            }else{
+//
+//                            }
+//                        }
+//                    }else if (num<0){
+//
+//                    }else{
+//
+//                    }
+//                }else{
+//
+//                }
+//            }else if (num<0){
+//                if (num > 0){
+//
+//                }else if (num<0){
+//
+//                }else{
+//
+//                }
+//            }else{
+//
+//            }
+//        }else if (num<0){
+//
+//        }else{
+//            if (num > 0){
+//
+//            }else if (num<0){
+//
+//            }else{
+//                if (num > 0){
+//                    if (num > 0){
+//
+//                    }else if (num<0){
+//
+//                    }else{
+//
+//                    }
+//                }else if (num<0){
+//
+//                }else{
+//
+//                }
+//
+//                if (num > 0){
+//
+//                }else if (num<0){
+//
+//                }else{
+//
+//                }
+//
+//                if (num > 0){
+//
+//                }else if (num<0){
+//
+//                }else{
+//
+//                }
+//            }
+//        }
+//
+//
+
+//        int x = 10;
+
+
+//        {
+//          int x = 10;
+//            System.out.println(x);
+//        }
+//            System.out.println(x);
+//
+//
+//
+//        System.out.println(x);
+//        if (true){
+//            System.out.println(x);
+//        }else{
+//            System.out.println(x);
+//        }
+//        System.out.println(x);
+
+
+//        if (true){
+//            int x = 1;
+//
+//            if (false){
+//                x = 10;
+//            }else{
+//                x = 20;
+//            }
+//        }
+
+
+//        int x = 55;
+//        boolean isPositive = false;
+//
+//        if (x>0){
+//            isPositive = true;
+//        } else {
+//            isPositive = false;
+//        }
+//
+//        System.out.print("Cislo : " + x );
+//        if (isPositive){
+//            System.out.println(" , polojitelnoe");
+//        }else{
+//            System.out.println(" , otricatelnoe");
+//        }
+
+
+//        // && (and)  || (or)
+//
+//        int age = 22;
+//        boolean isMan = false;
+//
+//        if (age>=18){
+//            if (isMan){
+//                System.out.println("Welcome");
+//            }else{
+//                System.out.println("Nelzya 1");
+//            }
+//        }else{
+//            System.out.println("Nelzya 2");
+//        }
+
+
+        // && (and)  || (or)
+
+//        int age = 22;
+//        boolean isMan = true;
+//
+//        if (age >= 18 || isMan) {
+//            System.out.println("Welcome");
+//        } else {
+//            System.out.println("Nelzya ");
+//        }
+
+        //21:20
+
+        //esli muwcina i >50    print ok
+        //esli muwcina i <50    print yes
+        //esli jenwina ili =70  print da
+
+        // && (and)  || (or)
+        //Выведи города :
+        // Баку если число положительное и четное
+        // Москва если число положительное и не четное
+        // Париж если число отрицательное и не четное
+        // город Лондон если число отрицательное и четное
+
+
+//        int num = 0;
+//
+//        if (num > 0 && num%2==0){
+//            System.out.println("Баку");
+//        }else if (num > 0 && num%2!=0){
+//            System.out.println("Москва");
+//        }else if (num < 0 && num%2!=0){
+//            System.out.println("Париж");
+//        }else if (num < 0 && num%2==0){
+//            System.out.println("Лондон");
+//        }else{
+//            System.out.println("ошибка");
+//        }
+
+//        int age = 10;
+//        boolean isMan = true;
+//
+//
+//        if (isMan && age>10){
+//            if (isMan && age>10){
+//                if (isMan && age>10){
+//
+//                }
+//            }
+//        }else{
+//            if (isMan && age>10){
+//                if (isMan && age>10){
+//                    if (isMan && age>10){
+//
+//                    }
+//                }
+//            }
+//        }
+
+//        boolean isWoman = true;
+//        System.out.println(isWoman);
+//        isWoman = !isWoman;
+//        System.out.println(isWoman);
+
+//        int a = 10;
+//        a = !a;//error
+
+
+//        boolean isGood = true;
+//        if (isGood || !isGood){
+//            isGood = !isGood;
+//            System.out.println("OK");
+//        }
+//
+//        System.out.println(isGood);//
+
+
+//        boolean isGood = false;
+//
+//        if (isGood )
+//        {
+//            System.out.println("DAAAAAAA");
+//        }
+
+//        System.out.println(isGood);
+
+        //DAAAAA TRUE
+        //DAAAAA FALSE
+
+//
+
+
+//        boolean isMan = false;
+//
+//
+//
+//        if (isMan)  System.out.println("daa");
+//        else {
+//            System.out.println("net");
+//            System.out.println("net");
+//            System.out.println("net");
+//            System.out.println("net");
+//            System.out.println("net");
+//            System.out.println("net");
+//        }
+
+
+//        if (true)
+//            System.out.println("1");
+//        else
+//            System.out.println("2");
+
+
+    }
+
+
+//      8 4 2 1
+//      0 1 0 1   5
+//      1 0 1 0   10
+//      1 1 1 1   10
+
+
+    //01010101010100101
+
+    // 0101 &
+    // 0011
+    // 0001     1
+    // 0101 &
+    // 0011
+    // 0001     1
+
+//        if (false || true || true || true || true || true && true){
+//            System.out.println("Da");
+//        }else{
+//            System.out.println("Net");
+//        }
+//    }
+}
