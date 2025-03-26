c4c08d79606de7f220fd2bba3ccd5bca2c06b2be
Dmitrii <sorokindm1try@yandex.ru>
2025-03-26 12:59:40 +0300

    classWork_9 1.3

--- src/homeWork_9/HomeWork_9.java
+++ src/homeWork_9/HomeWork_9.java
@@ -5,29 +5,38 @@ import java.util.Scanner;
 
 public class HomeWork_9 {
     public static void main(String[] args) {
+
+
+        // 2 задание
+
         System.out.println(" Введите любое число от 0 до 100: ");
 
         Scanner scanner = new Scanner(System.in);
 
         Random random = new Random();
         int randomNumber = random.nextInt(101);
-        int i=0;
+        int i = 0;
 
         while (true) {
             i++;
             int number = scanner.nextInt();
 
+            if (number > 100 || number < 0) {
+                System.out.println("Введите значение от 0 до 100 !!!");
+            }
+
             if (number == randomNumber) {
-                System.out.println("Угадал");
+                System.out.println("Вы угадали число). Это число " + number);
                 break;
             } else if (number > randomNumber) {
-                System.out.println("Вы ввели больше, чем загадал компьютер " + number);
+                System.out.println("Вы ввели больше, чем загадал компьютер.Число меньше, чем " + number);
             } else {
-                System.out.println("Вы ввели меньше, чем загадал компьютер " + number);
+                System.out.println("Вы ввели меньше, чем загадал компьютер. Число больше, чем " + number);
             }
 
 
         }
         System.out.println("Количество попыток: " + i);
+        scanner.close();
     }
 }
--- src/homeWork_9/HomeWork_9_1.java
+++ src/homeWork_9/HomeWork_9_1.java
@@ -11,26 +11,62 @@ public class HomeWork_9_1 {
 
         System.out.println("Введите любое число от 0 до 100: ");
         int number = scanner.nextInt();
+        int min = 0;
+        int max = 100;
 
         int count = 0;
 
-
-
+        if (number > max || number < min) {
+            System.out.println(" Некорректный ввод!!! Введите число от 0 до 100 ");
+            return;
+        }
 
         while (true) {
             count++;
-            int randomNumber = random.nextInt(101);
+
+
+            int randomNumber = (random.nextInt(max - min + 1) + min);
             System.out.println(randomNumber);
+
+
             if (number == randomNumber) {
-                System.out.println(" Компьютер угадал число) ");
+                System.out.println(" Компьютер угадал число) Это число " + number);
                 break;
 
             }
+            System.out.println("Если введенное число больше, то введите (1) или меньше, то введите (2) ");
+
+
+            int a = scanner.nextInt();
+
+            if (a == 1) {
+                min = randomNumber + 1;
+
+                System.out.println("Число больше, чем " + randomNumber);
+            } else if (a == 2) {
+                max = randomNumber - 1;
+                System.out.println("Число меньше чем " + randomNumber);
+            } else {
+                System.out.println("Неверный ввод!!! Введи 1 или 2");
+
+            }
+
         }
         System.out.println("Количество попыток: " + count);
+        scanner.close();
+
     }
 }
-            // Компьютер выдаёт случайные числа от 0 до 100
+
+
+
+
+
+
+
+
+
+
 
 
 
