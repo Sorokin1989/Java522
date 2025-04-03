a7dcb3026defb268fdb7dd064081071d1cd4768a
Dmitrii <sorokindm1try@yandex.ru>
2025-04-03 13:59:31 +0300

    homeWork_12 1.4

--- src/homeWork_12/HomeWork_12.java
+++ src/homeWork_12/HomeWork_12.java
@@ -1,5 +1,6 @@
 package homeWork_12;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class HomeWork_12 {
@@ -73,27 +74,55 @@ public class HomeWork_12 {
 
         // 4 Заполнить двумерный массив 3x4 числами от 1 до 12 и найти их среднее арифметическое.
 
- //      int[][] arrays = {{1, 2, 3, 4},
- //              {5, 6, 7, 8},
- //              {9, 10, 11, 12}};
+        //      int[][] arrays = {{1, 2, 3, 4},
+        //              {5, 6, 7, 8},
+        //              {9, 10, 11, 12}};
 
- //      for (int i = 0; i < arrays.length; i++) {
- //          System.out.println();
- //          for (int j = 0; j < arrays[i].length; j++) {
- //              System.out.print(arrays[i][j] + " ");
- //          }
- //      }
- //      double arithmetic = 0;
- //      int sum = 0;
- //      for (int i = 0; i < arrays.length; i++) {
- //          for (int j = 0; j < arrays[i].length; j++) {
- //              sum = (sum + arrays[i][j]);
- //              arithmetic = (double) sum / (arrays.length * arrays[i].length);
- //          }
- //      }
- //      System.out.print("\nСреднее арифметическое: " + arithmetic);
+        //      for (int i = 0; i < arrays.length; i++) {
+        //          System.out.println();
+        //          for (int j = 0; j < arrays[i].length; j++) {
+        //              System.out.print(arrays[i][j] + " ");
+        //          }
+        //      }
+        //      double arithmetic = 0;
+        //      int sum = 0;
+        //      for (int i = 0; i < arrays.length; i++) {
+        //          for (int j = 0; j < arrays[i].length; j++) {
+        //              sum = (sum + arrays[i][j]);
+        //              arithmetic = (double) sum / (arrays.length * arrays[i].length);
+        //          }
+        //      }
+        //      System.out.print("\nСреднее арифметическое: " + arithmetic);
 
+        // 5 Заполнить двумерный массив 2x5 случайными числами от 0 до 100 и вывести только чётные числа.
 
+        Random random = new Random();
+
+        int min = 0;
+        int max = 100;
+
+        int[][] arrays = new int[2][5];
+        System.out.print("Исходный массив: ");
+        for (int i = 0; i < arrays.length; i++) {
+            System.out.println();
+            for (int j = 0; j < arrays[i].length; j++) {
+                arrays[i][j] = random.nextInt(max - min + 1) + min;
+                System.out.print(arrays[i][j] + " ");
+            }
+
+        }
+        System.out.println();
+        System.out.print("\nТолько четные числа: ");
+        for (int i = 0; i < arrays.length; i++) {
+            System.out.println();
+            for (int j = 0; j < arrays[i].length; j++) {
+
+                if (arrays[i][j] % 2 == 0) {
+                    System.out.print(arrays[i][j] + " ");
+                }
+
+            }
+        }
 
     }
 }
