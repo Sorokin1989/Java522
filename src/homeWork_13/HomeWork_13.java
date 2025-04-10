6729161b5ae4e2e8db91dedf02f83afae74fbe10
Dmitrii <sorokindm1try@yandex.ru>
2025-04-10 13:30:42 +0300

    homeWork_13 1.0

---
+++ src/homeWork_13/HomeWork_13.java
@@ -0,0 +1,34 @@
+package homeWork_13;
+
+public class HomeWork_13 {
+    public static void main(String[] args) {
+        sayHi();
+        int num=square(5);
+        System.out.println(num);
+    }
+    //1. Метод sayHi()
+    //Принимает: ничего
+    //Возвращает: void
+    //Что делает: выводит "Привет!" в консоль
+
+    public static void sayHi() {
+        System.out.println("Привет!");
+    }
+
+    //2. Метод square(int number)
+    //Принимает: одно int число
+    //Возвращает: int
+    //Что делает: возвращает квадрат числа
+
+    public static int square (int number) {
+        int sq2=number*number;
+        return sq2;
+    }
+    // 3. Метод greetByName(String name)
+    //Принимает: строку name
+    //Возвращает: void
+    //Что делает: печатает "Привет, <имя>!"
+
+    public static void greetByName
+
+}
