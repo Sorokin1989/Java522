fb4c36be75c30a54417c7d2602762691ae4c3137
Dmitrii <sorokindm1try@yandex.ru>
2025-04-23 16:50:48 +0300

    homeWork_16_1.2

--- src/homeWork_16/HomeWork_16.java
+++ src/homeWork_16/HomeWork_16.java
@@ -21,24 +21,27 @@ public class HomeWork_16 {
 
         String str1 = "Dmitrii Sorokin";
         String str2 = "Dmitrii Sorokin";
-        String suffix="okinn";
-        String prefix="Dmi";
+        String suffix = "okinn";
+        String prefix = "Dmitrii";
+
+
 
         char[] arr1 = str1.toCharArray();
         char[] arr2 = str2.toCharArray();
-        char[] suff=suffix.toCharArray();
-        char[] pref=prefix.toCharArray();
+        char[] suff = suffix.toCharArray();
+        char[] pref = prefix.toCharArray();
 
         System.out.println(str1.equals(str2));
         System.out.println(myEquals(arr1, arr2));
 
         System.out.println(str1.endsWith("okinn"));
-        System.out.println(myEndsWith(arr1,suff));
+        System.out.println(myEndsWith(arr1, suff));
 
         System.out.println(str1.startsWith("Dmi"));
-        System.out.println(myStartsWith(arr1,pref));
+        System.out.println(myStartsWith(arr1, pref));
 
-
+        System.out.println(str1.replace('D', 'F'));
+        System.out.println(myReplace(arr1,'D','F'));
 
 
     }
@@ -57,7 +60,7 @@ public class HomeWork_16 {
         else return false;
     }
 
-    public static boolean myEndsWith(char[] str1,char[] suffix) {
+    public static boolean myEndsWith(char[] str1, char[] suffix) {
         int strLength = str1.length;
         int suffixLength = suffix.length;
 
@@ -71,13 +74,14 @@ public class HomeWork_16 {
             }
         }
 
-            return true;
+        return true;
 
     }
-    public static boolean myStartsWith(char[] str1,char[] prefix) {
+
+    public static boolean myStartsWith(char[] str1, char[] prefix) {
         int strLength = str1.length;
         int prefixLength = prefix.length;
-        if (prefixLength>strLength)
+        if (prefixLength > strLength)
             return false;
 
         for (int i = 0; i < prefixLength; i++) {
@@ -91,6 +95,18 @@ public class HomeWork_16 {
 
     }
 
+    public static char[] myReplace(char[] str1, char oldChar,char newChar) {
+        char[] arr1 = new char[str1.length];
+        for (int i = 0; i < arr1.length; i++) {
+            if (str1[i] == oldChar) {
+                arr1[i] = newChar;
+            } else {
+                arr1[i]=str1[i];
+            }
+
+        }
+        return arr1;
 
 
+    }
 }
