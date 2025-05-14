db3a372b93ce0a7a9ebc8178a862ce69617e4f7b
Dmitrii <sorokindm1try@yandex.ru>
2025-05-14 11:45:07 +0300

    lessonsTest 2

--- src/LessonsTest/LessonsTest.java
+++ src/LessonsTest/LessonsTest.java
@@ -15,11 +15,25 @@ public class LessonsTest {
 
         int[] array1 = {1, 2, 3};
         int[] array2 = {4, 5};
-        int[] arr3 = twoArr2(array1, array2);
+        int[] arr3 = twoArr(array1, array2);
         System.out.println(Arrays.toString(arr3));
+
+        //Задание 2: Создать массив из элементов двух массивов
+        //Описание:
+        //Создайте новый массив, который содержит все элементы двух исходных массивов в порядке их следования.
+        //
+        //Пример:
+        //Вход: [10, 20] и [30, 40]
+        //Выход: [10, 20, 30, 40]
+
+        int[] arr = {10, 20};
+        int[] arr2 = {30, 40};
+
+        int[] array3 = twoArr2(arr, arr2);
+        System.out.println(Arrays.toString(array3));
     }
 
-    public static int[] twoArr2(int[] array1, int[] array2) {
+    public static int[] twoArr(int[] array1, int[] array2) {
         int[] array3 = new int[array1.length + array2.length];
         for (int i = 0; i < array1.length; i++) {
             array3[i] = array1[i];
@@ -30,5 +44,17 @@ public class LessonsTest {
 
         return array3;
     }
+
+    public static int[] twoArr2(int[] arr, int[] arr2) {
+        int[] arr3 = new int[arr.length + arr2.length];
+        for (int i = 0; i < arr.length; i++) {
+            arr3[i] = arr[i];
+        }
+        for (int i = 0; i < arr2.length; i++) {
+            arr3[arr.length + i] = arr2[i];
+        }
+        return arr3;
+    }
+
 
 }
