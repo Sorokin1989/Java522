6a63a1b87db6b4a3bb8092f15b43d4083980d4e7
Dmitrii <sorokindm1try@yandex.ru>
2025-05-23 14:42:43 +0300

    HomeWork23 1.0

---
+++ src/homeWork_23/HomeWork_23.java
@@ -0,0 +1,134 @@
+package homeWork_23;
+
+public class HomeWork_23 {
+    public static void main(String[] args) {
+        //1. Финальный класс
+        //Задание:
+        //Создай финальный класс MathConstants, в котором будут финальные переменные PI и E.
+        //Попробуй создать от него наследника. Объясни, что произошло и почему.
+
+        // выходит ошибка (Cannot inherit from final 'homeWork_23.MathConstants')
+        //нельзя наследоваться от класса с ключевым словом final.
+        //final значит последний класс, у которого не может быть наследников.
+        //
+
+        //2. Переопределение метода (Overriding)
+        //Задание:
+        //Создай класс Animal с методом speak(), который выводит "Животное издает звук".
+        //Создай класс Dog, который наследует Animal и переопределяет метод speak() — пусть выводит "Собака лает".
+        //Создай объект Animal и Dog и вызови метод speak() у каждого.
+        //
+
+        Animal animal=new Animal();
+        Dog dog=new Dog();
+        animal.speak();
+        dog.speak();
+
+        //3. Наследование
+        //Задание:
+        //Создай класс Person с полями name и age.
+        //Создай подкласс Student, добавь поле university.
+        //Создай конструкторы и методы для вывода полной информации.
+        //Создай объект Student и выведи данные.
+
+        Student student=new Student("Дмитрий",20,"Московский Государственный Университет");
+        student.printInfo();
+        //
+        //4. Абстрактный класс
+        //Задание:
+        //Создай абстрактный класс Shape с абстрактным методом getArea() и обычным методом printInfo().
+        //Создай класс Circle, который наследует Shape и реализует метод getArea().
+        //Создай объект Circle и выведи площадь круга.
+        //
+        //
+        //5. Абстрактный метод + массив объектов
+        //Задание:
+        //Создай абстрактный класс Employee с методом calculateSalary().
+        //Создай два подкласса: FullTimeEmployee и PartTimeEmployee, каждый со своей реализацией зарплаты.
+        //Создай массив Employee[] и заполни его объектами разных классов. Выведи зарплату каждого.
+        //
+        //
+        //6. int[] arr = {9,1,7,2,8,4,5,0,2};
+        //
+        //Отсортировать массив
+        //Нельзя использовать готовое решение
+    }
+}
+final class MathConstants {
+    final double PI;
+    final double E;
+
+    MathConstants(double pi, double e) {
+        PI = pi;
+        E = e;
+    }
+}
+
+//class A extends MathConstants {
+//}
+
+class Animal {
+    public void speak() {
+        System.out.println("Животное издаёт звук");
+    }
+
+}
+
+class Dog extends Animal {
+    @Override
+    public void speak() {
+        System.out.println("Собака лает");
+    }
+}
+
+class Person {
+    String name;
+    int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+
+
+
+    }
+}
+
+class Student extends Person {
+     String university;
+
+    public String getUniversity() {
+        return university;
+    }
+
+    public void setUniversity(String university) {
+        this.university = university;
+    }
+
+    public Student(String name, int age, String university) {
+        super(name,age);
+        this.university=university;
+
+
+    }
+
+    public void printInfo() {
+        System.out.println(getName()+ " " + getAge() + " " + getUniversity());
+    }
+}
