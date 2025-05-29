9169d52f09914bcd97049252adeef58a35465e27
Dmitrii <sorokindm1try@yandex.ru>
2025-05-29 15:48:12 +0300

    LessonTestEnum 1.0

--- src/LessonsTest/LessonTest3.java
+++ src/LessonsTest/LessonTest3.java
@@ -1,100 +1,30 @@
 package LessonsTest;
 
-interface Vehicle {
-    default void start() {
-        System.out.println("Двигатель запустился");
-    }
-
-    default void stop() {
-        System.out.println("Двигатель остановился");
-    }
-
-    int getNumberOfWheels();
-}
-
-class Car implements Vehicle {
-    //  @Override
-    //  public void start() {
-    //  }
-//
-    //  @Override
-    //  public void stop() {
-//
-    //  }
-
-    @Override
-    public int getNumberOfWheels() {
-        return 4;
-    }
-}                           //(4 колеса)
-
-class Bike implements Vehicle {
-
-//    @Override
-//    public void start() {
-//
-//    }
-//
-//    @Override
-//    public void stop() {
-//
-//    }
-
-    @Override
-    public int getNumberOfWheels() {
-        return 2;
-    }
-}
-
-class Truck implements Vehicle {
-
-    //   @Override
-    //   public void start() {
-//
-    //   }
-//
-    //   @Override
-    //   public void stop() {
-//
-    //   }
-
-    @Override
-    public int getNumberOfWheels() {
-        return 6;
-    }
+enum Color {
+    RED, GREEN, BLUE, WHITE
 }
 
 
 public class LessonTest3 {
     public static void main(String[] args) {
-
-//Задание 3: Интерфейс Vehicle и классы транспортных средств
-//Описание:
-//
-//Создайте интерфейс Vehicle с методами:
-//
-//void start()
-//void stop()
-//int getNumberOfWheels()
-//Реализуйте классы:
-//
-//Car (4 колеса)
-//Bike (2 колеса)
-//Truck (больше 4 колес)
-//В каждом классе реализуйте методы, выводящие сообщение о запуске/остановке, а также возвращающие число колес.
-        Car car = new Car();
-        car.start();
-        car.stop();
-        System.out.println(car.getNumberOfWheels());
-        Bike bike = new Bike();
-        bike.start();
-        bike.stop();
-        System.out.println(bike.getNumberOfWheels());
-        Truck truck = new Truck();
-        truck.start();
-        truck.stop();
-        System.out.println(truck.getNumberOfWheels());
+        Color color = Color.WHITE;
+        if (color == Color.BLUE) {
+            System.out.println("This is BLUE");
+        } else if (color == Color.RED) {
+            System.out.println("This is RED");
+        } else if (color == Color.GREEN) {
+            System.out.println("This is GREEN");
+        } else if (color == Color.WHITE) {
+            System.out.println("This is WHITE");
+        }
     }
-
 }
+
+
+
+
+
+
+
+
 
