807d0b5f41820e7a227cd9276196d9f2831452bd
Dmitrii <sorokindm1try@yandex.ru>
2025-05-29 16:13:44 +0300

    LessonTestEnum 1.1

--- src/LessonsTest/LessonTest3.java
+++ src/LessonsTest/LessonTest3.java
@@ -4,19 +4,41 @@ enum Color {
     RED, GREEN, BLUE, WHITE
 }
 
+enum Day {
+    MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
+}
+
 
 public class LessonTest3 {
     public static void main(String[] args) {
-        Color color = Color.WHITE;
-        if (color == Color.BLUE) {
-            System.out.println("This is BLUE");
-        } else if (color == Color.RED) {
-            System.out.println("This is RED");
-        } else if (color == Color.GREEN) {
-            System.out.println("This is GREEN");
-        } else if (color == Color.WHITE) {
-            System.out.println("This is WHITE");
-        }
+
+//        Day day=Day.FRIDAY;
+//        System.out.println(day.ordinal());
+//        Day day=Day.FRIDAY;
+//String str=" ";
+//if (day==Day.FRIDAY) {
+//    System.out.println(" ");
+//}
+//        String str="MONDAY";
+//        Day day=Day.MONDAY;
+//        if (day.name().equals(str)) {
+//            System.out.println("This is MONDAY");
+//        }
+
+
+
+
+
+//        Color color = Color.WHITE;
+//        if (color == Color.BLUE) {
+//            System.out.println("This is BLUE");
+//        } else if (color == Color.RED) {
+//            System.out.println("This is RED");
+//        } else if (color == Color.GREEN) {
+//            System.out.println("This is GREEN");
+//        } else if (color == Color.WHITE) {
+//            System.out.println("This is WHITE");
+//        }
     }
 }
 
