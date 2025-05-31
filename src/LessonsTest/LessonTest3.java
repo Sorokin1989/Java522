e90786b37e38c2dd93fc7c7ca202eca1b6ac26d8
Dmitrii <sorokindm1try@yandex.ru>
2025-05-31 15:40:35 +0300

    LessonTest 1.1

--- src/LessonsTest/LessonTest3.java
+++ src/LessonsTest/LessonTest3.java
@@ -1,62 +1,102 @@
 package LessonsTest;
 
 
+//enum Direction {
+//    NORTH, SOUTH, EAST, WEST;
+//}
+//
+//class Robot {
+//    Direction direction;
+//
+//    public Robot() {
+//        this.direction = Direction.NORTH;
+//    }
+//
+//    public void turnLeft() {
+//        if (Direction.NORTH == direction) {
+//           direction= Direction.WEST;
+//        } else if (Direction.WEST == direction) {
+//            direction=Direction.SOUTH;
+//        } else if (Direction.SOUTH == direction) {
+//           direction=Direction.EAST;
+//        } else if (Direction.EAST == direction) {
+//           direction=Direction.NORTH;
+//        } else  return;
+//
+//    }
+//
+//    public void turnRight() {
+//        if (Direction.NORTH == direction) {
+//            direction=Direction.EAST;
+//        } else if (Direction.EAST == direction) {
+//           direction= Direction.SOUTH;
+//        } else if (Direction.SOUTH == direction) {
+//          direction= Direction.WEST;
+//        } else if (Direction.WEST == direction) {
+//         direction= Direction.NORTH;
+//        } else return;
+//    }
+//
+//    public Direction getDirection() {
+//        return direction;
+//    }
+//}
+
+
 enum Direction {
-    NORTH, SOUTH, EAST, WEST;
+    NORTH, SOUTH, EAST, WEST, NORTHEAST, NORTHWEST, SOUTHEAST, SOUTHWEST;
+
+    public Direction opposite() {
+        switch (this) {
+            case NORTH -> {
+                return SOUTH;
+            }
+            case SOUTH -> {
+                return NORTH;
+            }
+            case WEST -> {
+                return EAST;
+            }
+            case EAST -> {
+                return WEST;
+            }
+            case NORTHEAST -> {
+                return SOUTHWEST;
+            }
+            case SOUTHEAST -> {
+                return NORTHWEST;
+            }
+            case SOUTHWEST -> {
+                return NORTHEAST;
+            }
+            case NORTHWEST -> {
+                return SOUTHEAST;
+            }
+            default -> throw new IllegalStateException("Unexpected direction: " + this);
+        }
+    }
+
+
 }
 
-class Robot {
-    Direction direction;
-
-    public Robot() {
-        this.direction = Direction.NORTH;
-    }
-
-    public void turnLeft() {
-        if (Direction.NORTH == direction) {
-           direction= Direction.WEST;
-        } else if (Direction.WEST == direction) {
-            direction=Direction.SOUTH;
-        } else if (Direction.SOUTH == direction) {
-           direction=Direction.EAST;
-        } else if (Direction.EAST == direction) {
-           direction=Direction.NORTH;
-        } else  return;
-
-    }
-
-    public void turnRight() {
-        if (Direction.NORTH == direction) {
-            direction=Direction.EAST;
-        } else if (Direction.EAST == direction) {
-           direction= Direction.SOUTH;
-        } else if (Direction.SOUTH == direction) {
-          direction= Direction.WEST;
-        } else if (Direction.WEST == direction) {
-         direction= Direction.NORTH;
-        } else return;
-    }
-
-    public Direction getDirection() {
-        return direction;
-    }
-}
 
 public class LessonTest3 {
     public static void main(String[] args) {
 
-        Robot robot = new Robot();
-        System.out.println(robot.getDirection());
-        robot.turnRight();
-        System.out.println(robot.getDirection());
-        robot.turnRight();
-        robot.turnRight();
-        System.out.println(robot.getDirection());
-        robot.turnLeft();
-        System.out.println(robot.getDirection());
-        robot.turnLeft();
-        System.out.println(robot.getDirection());
+        Direction direction = Direction.NORTHEAST;
+        System.out.println(direction.opposite());
 
+//        Robot robot = new Robot();
+//        System.out.println(robot.getDirection());
+//        robot.turnRight();
+//        System.out.println(robot.getDirection());
+//        robot.turnRight();
+//        robot.turnRight();
+//        System.out.println(robot.getDirection());
+//        robot.turnLeft();
+//        System.out.println(robot.getDirection());
+//        robot.turnLeft();
+//        System.out.println(robot.getDirection());
 
 
     }
