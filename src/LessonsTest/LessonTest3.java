08e133b6bdd28afa87be2e1576e7e1b32e7501e3
Dmitrii <sorokindm1try@yandex.ru>
2025-06-01 13:36:18 +0300

    LessonTest new 1.2

--- src/LessonsTest/LessonTest3.java
+++ src/LessonsTest/LessonTest3.java
@@ -114,37 +114,51 @@ package LessonsTest;
 //    }
 //}
 
-enum Planet {
-    MERCURY(2440*1000,3.3e23), VENUS(6051.8*1000,4.87e24), EARTH(6371*1000,5.972e24 ), MARS(3390.6*1000,418e23);
+//enum Planet {
+//    MERCURY(2440*1000,3.3e23), VENUS(6051.8*1000,4.87e24), EARTH(6371*1000,5.972e24 ), MARS(3390.6*1000,418e23);
+//
+//   public static final double G = 6.67430e-11;
+//
+//    private final double R;
+//     private final double m;
+//
+//    Planet(double r, double m) {
+//        this.R = r;
+//        this.m = m;
+//    }
+//
+//    double getSurfaceGravity() {
+//        return G * m / (R*R);
+//    }
+//
+//}
+enum Weekday {
+    MONDAY(false), TUESDAY(false), WEDNESDAY(false),
+    THURSDAY(false), FRIDAY(false), SATURDAY(true), SUNDAY(true);
+   private final boolean flag;
 
-   public static final double G = 6.67430e-11;
-
-    private final double R;
-     private final double m;
-
-    Planet(double r, double m) {
-        this.R = r;
-        this.m = m;
+    Weekday(boolean flag) {
+        this.flag = flag;
     }
 
-    double getSurfaceGravity() {
-        return G * m / (R*R);
+    public boolean isWeekend() {
+        return flag;
     }
-
 }
 
 
 public class LessonTest3 {
     public static void main(String[] args) {
 
-        for (Planet planet:Planet.values()) {
-            System.out.println(planet.name() + " " + planet.getSurfaceGravity());
 
+        for (Weekday weekday : Weekday.values()) {
+            System.out.println(weekday + " " + weekday.isWeekend());
         }
 
-
-
-
+//        for (Planet planet:Planet.values()) {
+//            System.out.println(planet.name() + " " + planet.getSurfaceGravity());
+//
+//        }
 
 
 //        WeatherForecast weatherForecast=new WeatherForecast(Season.SPRING);
