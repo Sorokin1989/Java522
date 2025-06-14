cca5c68dd2853b412d62010074bf602a92b38188
Dmitrii <sorokindm1try@yandex.ru>
2025-06-14 17:23:19 +0300

    homeWork_28 1.0

---
+++ src/homeWork_28/HomeWork_28.java
@@ -0,0 +1,23 @@
+package homeWork_28;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HomeWork_28 {
+    public static void main(String[] args) {
+
+        Map<String,String> dictionary=new HashMap<>();
+
+        dictionary.put("dog", "собака");
+        dictionary.put("cat","кошка");
+        dictionary.put("cow","корова");
+        dictionary.put("lion","лев");
+        dictionary.put("goose","гусь");
+
+        do {
+
+
+        }
+        while (true);
+    }
+}
