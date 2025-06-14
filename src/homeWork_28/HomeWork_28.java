5719f4cf493381aad70217b7cf243e57bbac8ba1
Dmitrii <sorokindm1try@yandex.ru>
2025-06-14 18:42:55 +0300

    homeWork_28 1.1

--- src/homeWork_28/HomeWork_28.java
+++ src/homeWork_28/HomeWork_28.java
@@ -2,6 +2,7 @@ package homeWork_28;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class HomeWork_28 {
     public static void main(String[] args) {
@@ -15,6 +16,56 @@ public class HomeWork_28 {
         dictionary.put("goose","гусь");
 
         do {
+            System.out.println();
+            System.out.print("Введите слово: ");
+            Scanner scanner=new Scanner(System.in);
+            String word=scanner.nextLine();
+            word=word.toLowerCase();
+
+            if (word.equals("print")) {
+                int count = 1;
+                for (String key : dictionary.keySet()) {
+                    System.out.println(count++ + ") " + key + " (en)=> " + dictionary.get(key) + " (ru) ");
+                }
+            }else  if (dictionary.containsKey(word)) {
+                System.out.println(word + " (en)=> " + dictionary.get(word) + " (ru) ");
+            }
+              else if (dictionary.containsValue(word)) {
+
+                  for (String key:dictionary.keySet())
+                      if (dictionary.get(key).equals(word))
+                          System.out.println(word + " (ru)=> " + key + " (en) ");
+                  break;
+
+            } else
+                System.out.println("Нет такого слова! Хотите его добавить???" + "\n1 - Да\n2 - Нет");
+
+              int select=scanner.nextInt();
+
+              switch (select) {
+                  case 1:
+                      Scanner scanner1=new Scanner(System.in);
+                      String translate=scanner1.nextLine();
+                      translate=translate.toLowerCase();
+
+                      System.out.println("Слово переведено с \n" +  "1 - (en) => (ru)\n " +
+                              "2 - (ru) => (en)");
+                       select=scanner.nextInt();
+
+
+
+                      break;
+                  case 2:
+                      System.out.println("Слово не добавлено!");
+                      break;
+                  default:
+                      System.out.println("Некорректное значение!");
+              }
+
+
+
+
+
 
 
         }
