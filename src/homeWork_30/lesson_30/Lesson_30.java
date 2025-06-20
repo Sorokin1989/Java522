7bfc297b1fbb7e843b750f2aad0cb64ddf896cb9
Dmitrii <sorokindm1try@yandex.ru>
2025-06-20 16:33:55 +0300

    lessonWork_30 1.2

--- src/homeWork_30/lesson_30/Lesson_30.java
+++ src/homeWork_30/lesson_30/Lesson_30.java
@@ -6,15 +6,24 @@ import java.io.IOException;
 public class Lesson_30 {
     public static void main(String[] args)  {
 
-        File file=new File("text2.txt");
-        try {
-            if (file.createNewFile()) {
-                System.out.println("Файл создан!");
-            }
-            else System.out.println("Файл уже существует!");
+//      File file=new File("text2.txt");
+//        try {
+//            if (file.createNewFile()) {
+//                System.out.println("Файл создан!");
+//            }
+//            else System.out.println("Файл уже существует!");
+//
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
 
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        File file=new File("Dima");
+
+        if (file.mkdir()) {
+            System.out.println("Папка создана!");
         }
+        else System.out.println("Папка уже существует!");
+
+
     }
 }
