10f76d2bf21fca3a1a0df006a39b19159ea36280
Dmitrii <sorokindm1try@yandex.ru>
2025-06-24 12:19:40 +0300

    lesson_31 1.0

---
+++ src/lessonTest3/LessonTest_4.java
@@ -0,0 +1,32 @@
+package lessonTest3;
+
+
+interface Printable {
+    void print(String str);
+}
+
+class Test2 implements Printable {
+
+    @Override
+    public void print(String str) {
+        System.out.println(str);
+
+    }
+}
+
+
+public class LessonTest_4 {
+    public static void main(String[] args) {
+//        Test2 test2 = new Test2();
+//        test2.print("Dima");
+
+//        Printable printable=new Printable() {
+//            @Override
+//            public void print(String str) {
+//                System.out.println(str);
+//            }
+//        };
+//
+//        printable.print("Dima");
+    }
+}
