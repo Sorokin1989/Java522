15befea4c7e7e1edc5cbdaa4b8bae62808f980a6
Dmitrii <sorokindm1try@yandex.ru>
2025-06-24 12:40:10 +0300

    lesson_31 1.1

--- src/lessonTest3/LessonTest_4.java
+++ src/lessonTest3/LessonTest_4.java
@@ -14,9 +14,34 @@ class Test2 implements Printable {
     }
 }
 
+//interface Operationable {
+//    int calculate(int x, int y);
+//
+//}
+
+
+
 
 public class LessonTest_4 {
     public static void main(String[] args) {
+
+//        Operationable sum=(x,y)->
+//        {
+//           int rezult= x + y;
+//           rezult*=rezult;
+//           return rezult;
+//        };
+//        Operationable raznost=(x,y)->x-y;
+//        Operationable umnogenie=(x,y)->x*y;
+//        Operationable delenie=(x,y)->x/y;
+//
+//        System.out.println(sum.calculate(10,5));
+//        System.out.println(raznost.calculate(10,5));
+//        System.out.println(umnogenie.calculate(10,5));
+//        System.out.println(delenie.calculate(10,5));
+
+
+
 //        Test2 test2 = new Test2();
 //        test2.print("Dima");
 
@@ -27,6 +52,15 @@ public class LessonTest_4 {
 //            }
 //        };
 //
+//        printable.print("Dima");
+
+//        Printable printable=(str)->{
+//            System.out.println(str);
+//            System.out.println(str);
+//            System.out.println(str);
+//            System.out.println(str);
+//            System.out.println(str);
+//        };
 //        printable.print("Dima");
     }
 }
