826786d8a4cb2d92f7eadb7dc4d81f4b8a78a91c
Dmitrii <sorokindm1try@yandex.ru>
2025-06-26 13:52:11 +0300

    lesson_32 1.2

--- src/homeWork_32/Lesson_32.java
+++ src/homeWork_32/Lesson_32.java
@@ -1,12 +1,19 @@
 package homeWork_32;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Lesson_32 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         //Stream Api
         //1)создание стрима из значений
@@ -19,17 +26,41 @@ public class Lesson_32 {
 //        String[] arr = {"A1", "A2", "A3", "A4", "A5"};
 //        Stream<String> stream = (Arrays.stream(arr));
 
-        //2)создание стрима из collection
-        List<String> list = new ArrayList<>();
-        list.add("a1");
-        list.add("a2");
-        list.add("a3");
-        list.add("a4");
-        list.add("a5");
-        list.add("a6");
+        //3)создание стрима из collection
+//        List<String> list = new ArrayList<>();
+//        list.add("a1");
+//        list.add("a2");
+//        list.add("a3");
+//        list.add("a4");
+//        list.add("a5");
+//        list.add("a6");
+//
+//        Stream<String> stream = list.stream();
+//        System.out.println(stream.collect(Collectors.toList()));
 
-        Stream<String> stream = list.stream();
-        System.out.println(stream.collect(Collectors.toList()));
+       // 4)создание стрима из file;
+
+//        File file=new File("data.txt");
+//        file.createNewFile();
+//        try {
+//            PrintWriter printWriter=new PrintWriter(file);
+//            printWriter.println("a1");
+//            printWriter.println("a2");
+//            printWriter.println("a3");
+//            printWriter.println("a4");
+//            printWriter.println("a5");
+//            printWriter.close();
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//try{
+//    Stream<String> stream= Files.lines(Paths.get(file.getAbsolutePath()));
+//    System.out.println(stream.collect(Collectors.toList()));
+//} catch (IOException e) {
+//    throw new RuntimeException(e);
+//}
+        //5)создание стрима из file;
+
 
 
     }
