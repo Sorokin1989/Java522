1434ea1f4406434926561b1e8cfefe2f47d0060c
Dmitrii <sorokindm1try@yandex.ru>
2025-06-27 14:25:47 +0300

    homeWork_32 1.9

--- src/homeWork_32/HomeWork_32.java
+++ src/homeWork_32/HomeWork_32.java
@@ -2,6 +2,7 @@ package homeWork_32;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class HomeWork_32 {
@@ -39,7 +40,7 @@ public class HomeWork_32 {
 //Добавь префикс "backup_" ко всем названиям файлов.
         List<String> files = Arrays.asList("photo.jpg", "doc.txt", "archive.zip");
 
-        files=files.stream().map(x->"backup_" + x).toList();
+        files = files.stream().map(x -> "backup_" + x).toList();
         System.out.println(files);
 
 
@@ -48,9 +49,8 @@ public class HomeWork_32 {
 //Есть List<String> phrases = Arrays.asList("Java is", "fun to", "learn and", "use!").
 //Раздели все фразы на отдельные слова с помощью flatMap.
         List<String> phrases = Arrays.asList("Java is", "fun to", "learn and", "use!");
-        phrases=phrases.stream().flatMap(x->Arrays.stream(x.split(" "))).toList();
+        phrases = phrases.stream().flatMap(x -> Arrays.stream(x.split(" "))).toList();
         System.out.println(phrases);
-
 
 
 //
@@ -58,7 +58,7 @@ public class HomeWork_32 {
 //6)Проверка наличия большого числа
 //Дан список List<Integer> numbers.
 //Используй anyMatch, чтобы проверить, есть ли число больше 1000.
-        List<Integer> numberss=new ArrayList<>();
+        List<Integer> numberss = new ArrayList<>();
         numberss.add(1000);
         numberss.add(100);
         numberss.add(10000);
@@ -68,30 +68,74 @@ public class HomeWork_32 {
         numberss.add(1580);
         numberss.add(45);
 
-        System.out.println(numberss.stream().anyMatch(x->x>1000));
+        System.out.println(numberss.stream().anyMatch(x -> x > 1000));
 
 //
 //7)Поиск самого короткого слова
 //Дан список слов. Найди слово с минимальной длиной с помощью min.
 
-        List<String> stringList=new ArrayList<>();
+        List<String> stringList = new ArrayList<>();
         stringList.add("Dima");
         stringList.add("Nikolai");
         stringList.add("Roman");
         stringList.add("Evgenii");
         stringList.add("Ola");
 
-        System.out.println(stringList.stream().min((x,y)->x.length()-y.length()));
+        System.out.println(stringList.stream().min((x, y) -> x.length() - y.length()));
+        System.out.println(stringList.stream().min(Comparator.comparingInt(String::length)).orElse("Список пустой"));
 
 
 //
 //8)Сумма чётных чисел
 //Дан список чисел. Найди сумму только чётных чисел с помощью reduce.
+
+
+        List<Integer> integerList = new ArrayList<>();
+        integerList.add(12);
+        integerList.add(23);
+        integerList.add(35);
+        integerList.add(46);
+        integerList.add(54);
+        integerList.add(12);
+        integerList.add(88);
+        integerList.add(35);
+        integerList.add(77);
+        integerList.add(56);
+
+        System.out.println(integerList.stream().filter(x->x%2==0).reduce((x,y)->x+y).orElseThrow());
+       // System.out.println(integerList.stream().filter(x->x%2==0).reduce(Integer::sum).get());
+
 //
 //9)Подсчёт слов длиной > 5
 //Дан список слов. Посчитай, сколько слов имеют длину больше 5 с помощью count.
+
+        List<String> words=new ArrayList<>();
+        words.add("Москва");
+        words.add("Новосибирск");
+        words.add("Казань");
+        words.add("Чебоксары");
+        words.add("Чебоксары");
+        words.add("Омск");
+        words.add("Новгород");
+
+        System.out.println(words.stream().filter(x->x.length()>5).count());
+
+
 //
 //10)Найди первую строку, начинающуюся с буквы 'A'
 //Используй findFirst для поиска строки, начинающейся на "A" из списка строк.
+
+        List<String> lists=new ArrayList<>();
+        lists.add("Anna");
+        lists.add("Olga");
+        lists.add("Andrew");
+        lists.add("Dmitrii");
+        lists.add("Nikolai");
+
+        System.out.println(lists.stream().filter(x->x.contains("A")).findFirst().orElse("Такого слова нет!"));
+
+
+
+
     }
 }
