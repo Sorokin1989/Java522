6bb99738f173d8fc772d77aa2022deed174d7272
Dmitrii <sorokindm1try@yandex.ru>
2025-06-30 15:21:46 +0300

    lessonWork 2.9

--- src/TestLessons/LessonTest.java
+++ src/TestLessons/LessonTest.java
@@ -31,20 +31,50 @@ public class LessonTest {
         //Фильтрация и преобразование чисел
         //Дан список List<Integer> nums = Arrays.asList(3, 7, 2, 9, 4);
         //Оставить только четные числа и умножить их на 10.
+
+        List<Integer> nums = Arrays.asList(3, 7, 2, 9, 4);
+
+        System.out.println(nums.stream().filter(x->x%2==0).map(x->x*10).toList());
+
+
         //
         //Объединение строк с разделителем
         //Дан список List<String> words = Arrays.asList("Java", "is", "awesome");
         //Объединить все слова в одну строку через пробел.
+
+        List<String> words = Arrays.asList("Java", "is", "awesome");
+
+        System.out.println(words.stream().collect(Collectors.joining(" ")));
+
+
         //
         //Задания на терминальные методы
         //Проверка отсутствия отрицательных чисел
         //Дан список numbers. Использовать noneMatch, чтобы проверить, что отрицательных чисел нет.
+
+
+        List<Integer> numbers=new ArrayList<>();
+
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(10);
+        numbers.add(14);
+        numbers.add(134);
+
+        System.out.println(numbers.stream().noneMatch(x->x<0));
+
         //
         //Поиск максимальной длины слова
         //Дан список слов. Найти длину самого длинного слова.
+
+        System.out.println(words.stream().max((x,y)->x.length()-y.length()).orElse(String.valueOf(0)));
+
         //
         //Подсчет суммы всех чисел
         //Дан список чисел. Посчитать сумму всех элементов с помощью reduce.
+
+
+
         //
         //Подсчет количества уникальных слов
         //Дан список слов с возможными повторениями. Посчитать количество уникальных слов.
