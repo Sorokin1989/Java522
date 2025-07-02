75ac7a59e80f606c1066dcfb02dc114eaaf345be
Dmitrii <sorokindm1try@yandex.ru>
2025-07-02 11:51:17 +0300

    classWork_33 1.0

---
+++ src/ClassWork_32/ClassWork_32.java
@@ -0,0 +1,26 @@
+package ClassWork_32;
+
+import java.io.IOException;
+
+public class ClassWork_32 {
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        Thread mainThread = Thread.currentThread();
+        System.out.println(mainThread.getName());
+
+
+
+//        System.out.println("start");
+//        for (int i = 0; i < 10; i++) {
+//            File file=new File("text" + i +  ".txt");
+//            file.createNewFile();
+//
+//        }
+//
+//        Thread.sleep(10000);
+//
+//        System.out.println("End");
+
+
+    }
+}
