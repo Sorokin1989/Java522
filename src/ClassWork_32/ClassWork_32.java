cdd84b4544a1df54d4ff7583c38230eec2b0d469
Dmitrii <sorokindm1try@yandex.ru>
2025-07-02 13:24:36 +0300

    classWork_33 1.1

--- src/ClassWork_32/ClassWork_32.java
+++ src/ClassWork_32/ClassWork_32.java
@@ -2,12 +2,59 @@ package ClassWork_32;
 
 import java.io.IOException;
 
+
+
+class MyThread extends Thread {
+  public void run() {
+      this.setName("MyThread");
+      System.out.println("Start " + this.getName());
+      System.out.println("Task done");
+      System.out.println("End " + this.getName());
+  }
+}
+
 public class ClassWork_32 {
     public static void main(String[] args) throws IOException, InterruptedException {
 
-        Thread mainThread = Thread.currentThread();
-        System.out.println(mainThread.getName());
+//        Thread mainThread = Thread.currentThread();
 
+      //  mainThread.stop();
+      //  mainThread.suspend();
+       // mainThread.resume()
+
+
+
+
+
+     //   System.out.println(mainThread.isInterrupted());// поток был прерван или нет
+       // Thread.interrupted();
+
+
+//        mainThread.setName("This is main Thread");
+//        System.out.println(mainThread.getName());
+//        System.out.println(mainThread.threadId());
+//        mainThread.setPriority(10);
+//        System.out.println(mainThread.getPriority());
+//        System.out.println(mainThread.getState());
+//
+//        mainThread.run();
+//        mainThread.start();
+
+//
+//        mainThread.join(); // ждет выполнение потока
+//        mainThread.join(5000);
+
+//        System.out.println(mainThread.isAlive()); // поток жив или нет
+//
+//        mainThread.interrupt(); // прерывает поток
+
+
+        // new новый поток
+        //Runnable  поток работает в данный момент
+        //Blocked поток заблокирован
+        //Waiting поток ожидает
+        //TimedWaiting ожидает по времени
+        //Terminated поток завершен
 
 
 //        System.out.println("start");
