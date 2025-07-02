67f489eaecd0d00b0df90622023ec79d2758e2ab
Dmitrii <sorokindm1try@yandex.ru>
2025-07-02 14:30:24 +0300

    classWork_33 1.2

--- src/ClassWork_32/ClassWork_32.java
+++ src/ClassWork_32/ClassWork_32.java
@@ -3,31 +3,154 @@ package ClassWork_32;
 import java.io.IOException;
 
 
+//class MyThread extends Thread {
+//
+//    private String rezult;
+//  public void run() {
+//      this.setName("MyThread");
+//      System.out.println("Start " + this.getName());
+//      for (int i = 0; i < 10; i++) {
+//          try {
+//              Thread.sleep(2000);
+//          } catch (InterruptedException e) {
+//              throw new RuntimeException(e);
+//          }
+//          System.out.print(i + " ");
+//
+//      }
+//      System.out.println("\nEnd " + this.getName());
+//      rezult="Good task";
+//  }
+//
+//    public String getRezult() {
+//        return rezult;
+//    }
+//
+//    public void setRezult(String rezult) {
+//        this.rezult = rezult;
+//    }
+//}
 
-class MyThread extends Thread {
-  public void run() {
-      this.setName("MyThread");
-      System.out.println("Start " + this.getName());
-      System.out.println("Task done");
-      System.out.println("End " + this.getName());
-  }
+class MyThread implements Runnable {
+
+    private String rezult;
+
+    public void run() {
+        System.out.println("Start myThread");
+        for (int i = 0; i < 10; i++) {
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.print(i + " ");
+
+        }
+        System.out.println("End myThread");
+        rezult = "Good task";
+    }
+
+    public String getRezult() {
+        return rezult;
+    }
+
+    public void setRezult(String rezult) {
+        this.rezult = rezult;
+    }
 }
 
 public class ClassWork_32 {
     public static void main(String[] args) throws IOException, InterruptedException {
 
+        System.out.println("Start main");
+
+//                System.out.println("Start myThread");
+//                for (int i = 0; i < 10; i++) {
+//                    try {
+//                        Thread.sleep(2000);
+//                    } catch (InterruptedException e) {
+//                        throw new RuntimeException(e);
+//                    }
+//                    System.out.print(i + " ");
+//
+//                }
+//
+//                System.out.println("End myThread");
+//            }
+//        };
+        Thread thread = new Thread(() -> {
+            System.out.println("Start myThread");
+            for (int i = 0; i < 10; i++) {
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.print(i + " ");
+
+            }
+
+            System.out.println("End myThread");
+        });
+        thread.start();
+
+        System.out.println("End main");
+
+
+//        System.out.println("Start main");
+//        Runnable runnable = new Runnable() {
+//            @Override
+//            public void run() {
+//
+//                System.out.println("Start myThread");
+//                for (int i = 0; i < 10; i++) {
+//                    try {
+//                        Thread.sleep(2000);
+//                    } catch (InterruptedException e) {
+//                        throw new RuntimeException(e);
+//                    }
+//                    System.out.print(i + " ");
+//
+//                }
+//
+//                System.out.println("End myThread");
+//            }
+//        };
+//
+//
+//                Thread thread = new Thread(runnable);
+//                thread.start();
+//
+//                System.out.println("End main");
+
+
+//        System.out.println("Start main");
+//        MyThread myThread=new MyThread();
+//
+//       Thread thread=new Thread(myThread);
+//       thread.start();
+//
+//        System.out.println("End main");
+
+//        System.out.println("Start main");
+//
+//       thread.start();
+//
+//
+//        System.out.println("End main");
+//        thread.join();
+//
+//        System.out.println(thread.getRezult());
+
 //        Thread mainThread = Thread.currentThread();
 
-      //  mainThread.stop();
-      //  mainThread.suspend();
-       // mainThread.resume()
+        //  mainThread.stop();
+        //  mainThread.suspend();
+        // mainThread.resume()
 
 
-
-
-
-     //   System.out.println(mainThread.isInterrupted());// поток был прерван или нет
-       // Thread.interrupted();
+        //   System.out.println(mainThread.isInterrupted());// поток был прерван или нет
+        // Thread.interrupted();
 
 
 //        mainThread.setName("This is main Thread");
