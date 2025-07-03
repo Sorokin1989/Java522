f47fd08e5345dd80bc487bcea56a12a4d5f8bc58
Dmitrii <sorokindm1try@yandex.ru>
2025-07-03 18:03:57 +0300

    ExaminationWork_1.0

---
+++ src/ExaminationWork/ExaminationWork.java
@@ -0,0 +1,14 @@
+package ExaminationWork;
+
+public class ExaminationWork {
+    public static void main(String[] args) {
+
+        System.out.println("------------------");
+        System.out.println("|Телефонная книга|\n" +
+                           "|1 - Sign in     |\n" +
+                           "|2 - Sign up     |\n" +
+                           "|3 - EXIT        |");
+        System.out.println("------------------");
+
+    }
+}
