650a8db32ab50099f2c4843caf21e3ceeaa899ba
Dmitrii <sorokindm1try@yandex.ru>
2025-10-25 13:20:35 +0300

    game 3.0

--- Java Script/work5/Homework5/script.js
+++ Java Script/work5/Homework5/script.js
@@ -25,33 +25,66 @@ for (let i = 0; i < countBox; i++) {
 
 function driveBox() {
     let boxes = document.querySelectorAll('.box');
-    let nav=document.querySelector('.nav');
+    // let box=document.querySelector('.box');
+    let conteiner=document.querySelector('.conteiner');
+    // let nav=document.querySelector('.nav');
     //   const navHeight = nav ? nav.offsetHeight : 0;
+
     
+    // const maxY=85;
+    // const maxX=92;
     boxes.forEach(box => {
-        // box.style.position = 'absolute';
-        box.style.left = Math.random() * 92 + '%';
+        box.style.position = 'absolute';
+        const maxX=conteiner.offsetWidth-box.offsetWidth;
+       const maxY=conteiner.offsetHeight-box.offsetHeight;
+       
+        box.style.left = Math.random() * maxX + 'px';
         // box.style.top = (Math.random() * 85) + navHeight + 'px';
-         box.style.top = Math.random() * 85  + '%';
-        box.vx = (Math.random() - 0.5) * 1;
-        box.vy = (Math.random() - 0.5) * 1;
+         box.style.top = Math.random() * maxY  + 'px';
+        box.vx = (Math.random() - 0.5) * 2;
+        box.vy = (Math.random() - 0.5) * 2;
     });
     
     (function move() {
         boxes.forEach(box => {
-            let x = parseFloat(box.style.left) + box.vx;
-            let y = parseFloat(box.style.top) + box.vy;
+            // let x = parseFloat(box.style.left) + box.vx;
+            // let y = parseFloat(box.style.top) + box.vy;
+      let x = parseFloat(box.style.left);
+      let y = parseFloat(box.style.top);
+
+            x += box.vx;
+            y += box.vy;
+
+       const maxX = conteiner.offsetWidth - box.offsetWidth;
+       const maxY = conteiner.offsetHeight - box.offsetHeight;
+
+            x = Math.max(0, Math.min(x, maxX));
+            y = Math.max(0, Math.min(y, maxY));
             
-            if (x < 0 || x > 92) box.vx = -box.vx;
-            // if (y < navHeight || y > conteiner.offsetHeight-50) box.vy = -box.vy;
-            if (y < 0 || y > 85) box.vy = -box.vy;
+            if (x <= 0 || x >= maxX) box.vx = -box.vx;
+            if (y <= 0 || y >= maxY) box.vy = -box.vy;
             
-            box.style.left = x + '%';
-            box.style.top = y + '%';
+            box.style.left = x + 'px';
+            box.style.top = y + 'px';
         });
         requestAnimationFrame(move);
     })();
 }
 
+
+
+function deleteBox() {
+document.querySelector('.conteiner').addEventListener('click',function(event){
+    if(event.target.className=='box'){
+        event.target.remove()
+    }
+})   
+}
+
+
+
+
+
 addBox();
 driveBox();
+deleteBox()
