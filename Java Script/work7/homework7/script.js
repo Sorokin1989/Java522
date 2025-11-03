18a079822fb2529e1bd5ede196b37bcfa6363f49
Dmitrii <sorokindm1try@yandex.ru>
2025-11-03 14:17:53 +0300

    homework7

--- Java Script/work7/homework7/script.js
+++ Java Script/work7/homework7/script.js
@@ -1,34 +1,29 @@
-let myForm=document.forms.toDoForm;
-let list=document.querySelector('.list');
-let btn=document.querySelector('.clearAll');
-let btn2=document.querySelector('.clear');
-let container=document.querySelector('.container');
-let zoomBtn=document.querySelector('.zoom-text');
-let zoomBtn2=document.querySelector('.zoom-text2');
-let countIncrease=1;
-let countDecrease=1;
-let defaultSize=12;
+let myForm = document.forms.toDoForm;
+let list = document.querySelector('.list');
+let btn = document.querySelector('.clearAll');
+let btn2 = document.querySelector('.clear');
+let container = document.querySelector('.container');
+let zoomBtn = document.querySelector('.zoom-text');
+let zoomBtn2 = document.querySelector('.zoom-text2');
+let countIncrease = 1;
+let countDecrease = 1;
+let defaultSize = 12;
 let canDecrease = false;
+let message3 = document.querySelector('.message3');
+// let title = myForm.title.value;
 
 
-myForm.addEventListener('submit',function(event) {
+myForm.addEventListener('submit', function (event) {
     event.preventDefault();
-    // console.log('hello');
-    let title=myForm.title.value;
-    console.log(title);
-    // list.insertAdjacentHTML('beforeend',` <li class="list-group-item">${title}</li>`)
-// list.innerText+=title;
-if(title.trim()){
-    let li=document.createElement('li');
-li.className='list-group-item item';
-li.innerHTML=`<span class="message">${title}</span>` + '<div><i class="check">✔️</i>  <i class="removeBtn">❌</i></div>'
-                                    ;
-// li.innerText=title;
-list.appendChild(li);
+    let title = myForm.title.value;
 
-}
-// myForm.title.value='';
-    myForm.reset();    
+    if (title.trim()) {
+        let li = document.createElement('li');
+        li.className = 'list-group-item item';
+        li.innerHTML = `<span class="message">${title}</span>` + '<div><i class="check">✔️</i>  <i class="removeBtn">❌</i></div>';
+        list.appendChild(li);
+    }
+    myForm.reset();
 })
 
 
@@ -38,161 +33,139 @@ list.appendChild(li);
 
 
 
-btn.addEventListener('click',function(){
-list.innerHTML='';
+btn.addEventListener('click', function () {
+    list.innerHTML = '';
 })
 
-list.addEventListener('click',function(event) {
-    if(event.target.classList.contains('removeBtn')){
-console.log('remove');
-event.target.parentElement.parentElement.remove();
+list.addEventListener('click', function (event) {
+        if (event.target.classList.contains('removeBtn')) {
+            event.target.parentElement.parentElement.remove();
+        } else if (event.target.classList.contains('check')) {
+            if (event.target.parentElement.parentElement.classList.contains('checkBtn')) {
+                event.target.parentElement.parentElement.classList.remove('checkBtn')
+            } else {
+                event.target.parentElement.parentElement.classList.add('checkBtn')
+            }
+        } else if (event.target.classList.contains('item')) {
+
+            let wasSelected = event.target.classList.contains('item2');
+            document.querySelectorAll('.item2').forEach(x => x.classList.remove('item2'))
+
+            if (!wasSelected) {
+                event.target.classList.add('item2');
+            }
+
+
+
+        } else if (event.target.classList.contains('message')) {
+            if (event.target.classList.contains('message2')) {
+                event.target.classList.remove('message2');
+            } else {
+                event.target.classList.add('message2')
+            }
+        }
+
+
     }
-
- else if (event.target.classList.contains('check')) {
-        if(event.target.parentElement.parentElement.classList.contains('checkBtn')){
-               event.target.parentElement.parentElement.classList.remove('checkBtn')
-    } else {
-        event.target.parentElement.parentElement.classList.add('checkBtn')
-}
- }
-
-   else if(event.target.classList.contains('item')) {
-
- let wasSelected = event.target.classList.contains('item2');
- document.querySelectorAll('.item2').forEach(x=>x.classList.remove('item2'))
-
- if (!wasSelected) {
-        event.target.classList.add('item2');
-    }
-
-
-
-}
-
-// if (event.target.classList.contains('item')) {
-//  document.querySelectorAll('.item2').forEach(x=>x.classList.remove('item2'))
-//  event.target.classList.add('item2')
-// }
-
- else if(event.target.classList.contains('message')) {
-    // event.target.classList.toggle('message2')
-    console.log('hello');
-    
-
-    if(event.target.classList.contains('message2')){
-        event.target.classList.remove('message2');
-    }else{
-        event.target.classList.add('message2')
-    }
-}
-    
-
-}
 
 
 )
 
 
-//    if (event.target.classList.contains('item')) {
-//     document.querySelectorAll('.item2').forEach(x=>x.classList.remove('item2'))
-//     event.target.classList.add('item2')
-// }
+container.addEventListener('click', function (event) {
+
+    if (event.target.classList.contains('clear')) {
+        const itemWithClass = event.target.parentElement.nextElementSibling.nextElementSibling.querySelector('.item2');
+// const itemWithClass = document.querySelector('.item2');
+        if (list.children.length === 0) {
+            message3.innerHTML = '<p>Список пуст!</p>';
+            setTimeout(() => {
+                message3.innerText = '';
+            }, 3000)
+            return;
+
+        }
+        if (itemWithClass) {
+            itemWithClass.remove();
+            message3.innerHTML = ''
+
+            if (list.children.length == 0) {
+                message3.innerHTML = '<p>Список пуст!</p>';
+                setTimeout(() => {
+                    message3.innerText = '';
+                }, 3000)
+            }
+
+        } else {
+
+            message3.innerHTML = '<p>Элемент не выбран!</p>';
+            setTimeout(() => {
+                message3.innerText = '';
+            }, 3000)
+        }
 
 
-
-
-
-container.addEventListener('click',function(event) {
- 
-    if(event.target.classList.contains('clear')) {
-        // list.querySelector('.item2')
-//  console.log('hello');
- const itemWithClass = event.target.parentElement.nextElementSibling.querySelector('.item2');
-if (itemWithClass) {
-    console.log('hello');
-    
-   itemWithClass.remove();
-    
-}
-
-// if(event.target.classList.contains('.zoom-text')){
-//     //  event.target.classList.contains('message').
-//     console.log('hello');
-    
-//     increaseText();
-// }
-}
+    }
 })
 
-
 function increaseText() {
-    const messages=document.querySelectorAll('.message');
+    const messages = document.querySelectorAll('.message');
 
-   let shouldIncreaseCounter = false;
- messages.forEach(message=>{
-     const currentSize = parseFloat(window.getComputedStyle(message).fontSize);
-//   countDecrease=1;
+    let shouldIncreaseCounter = false;
+    messages.forEach(message => {
+        const currentSize = parseFloat(window.getComputedStyle(message).fontSize);
 
-if(Math.abs((currentSize + 2*countIncrease)-defaultSize)<=3){
 
-message.style.fontSize=defaultSize + 'px';
-countIncrease=1;
-}else{
+        if (Math.abs((currentSize + 2 * countIncrease) - defaultSize) <= 3) {
 
-  message.style.fontSize = (currentSize + 2*countIncrease) + 'px';
-    shouldIncreaseCounter = true;
+            message.style.fontSize = defaultSize + 'px';
+            countIncrease = 1;
+        } else {
 
-}
+            message.style.fontSize = (currentSize + 2 * countIncrease) + 'px';
+            shouldIncreaseCounter = true;
+
+        }
 
     });
-      if (shouldIncreaseCounter) {
+    if (shouldIncreaseCounter) {
         countIncrease++;
-          canDecrease = true;
+        canDecrease = true;
     }
 }
 
 
 function decreaseText() {
 
-if(!canDecrease) return;
+    if (!canDecrease) return;
 
 
-  const messages = document.querySelectorAll('.message');
-  let shouldDecreaseCounter = false;
-   messages.forEach(message=>{
-     const currentSize = parseFloat(window.getComputedStyle(message).fontSize);
+    const messages = document.querySelectorAll('.message');
+    let shouldDecreaseCounter = false;
+    messages.forEach(message => {
+        const currentSize = parseFloat(window.getComputedStyle(message).fontSize);
 
 
-if(Math.abs((currentSize - 2*countDecrease)-defaultSize)<=3){
-message.style.fontSize=defaultSize + 'px';
-countDecrease=1;
-}else{
-  message.style.fontSize = Math.max(16,currentSize - 2*countDecrease) + 'px';
-  shouldDecreaseCounter=true;
-}
-        });
-      if (shouldDecreaseCounter) {
+        if (Math.abs((currentSize - 2 * countDecrease) - defaultSize) <= 3) {
+            message.style.fontSize = defaultSize + 'px';
+            countDecrease = 1;
+        } else {
+            message.style.fontSize = Math.max(16, currentSize - 2 * countDecrease) + 'px';
+            shouldDecreaseCounter = true;
+        }
+    });
+    if (shouldDecreaseCounter) {
         countDecrease++;
     }
 
 }
 
-
-
-
-
-// function resetCounters() {
-//     countIncrease=1;
-//     countDecrease=1;
-// }
-
-
-zoomBtn.addEventListener('click',function() {
+zoomBtn.addEventListener('click', function () {
     increaseText();
 });
 
 
 
-zoomBtn2.addEventListener('click',function() {
+zoomBtn2.addEventListener('click', function () {
     decreaseText();
 });
