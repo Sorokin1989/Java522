65dc14ac5b0b61ed7f83dd81fbca9300b018d05f
Dmitrii <sorokindm1try@yandex.ru>
2025-11-09 17:44:38 +0300

    examWork6.0

--- Java Script/Examination work JS/script.js
+++ Java Script/Examination work JS/script.js
@@ -33,6 +33,23 @@
 
 
 
+async function searchDetail(id){
+try{
+     console.log('🔄 Запрашиваем детали для ID:',id);
+    let data= await fetch(`https://www.omdbapi.com/?i=${id}&apikey=12b53682`);
+let response= await data.json();
+return response;
+
+}catch(error){
+    console.log('error', error);
+    
+    return null;
+}
+
+}
+
+
+
 
 
 
@@ -40,6 +57,8 @@
 let allCards=document.querySelector('.allCards');
  let searchForm=document.forms.searchForm;
  let searchBtn=document.querySelector('#button-addon2');
+ let detailsBtn=document.querySelector('.detailsBtn');
+ let detalies=document.querySelector('.detalies')
 
 
 searchBtn.addEventListener('click', async function () {
@@ -63,23 +82,87 @@ if(title.trim()){
                          <p class="year-text">${movie.Year}</p>
                      </div>
                      <div class="button-text ">
-                         <button type="button" class="btn btn-primary w-100 details-text">Details</button>
+                         <button type="button" class="btn btn-primary w-100 detailsBtn" data-imdbid="${movie.imdbID}">Details</button>
                      </div>
              </div>
          </div>`
  }
-
-
-
-
-
-
-
- 
 }
 
     searchForm.reset();
 })
+
+
+
+ document.addEventListener('click', async function(event){    
+    if(event.target.classList.contains('detailsBtn')){  
+console.log('кнопка нажата');
+
+
+  
+       let id= event.target.dataset.imdbid;
+       console.log('IMDb ID:', id);
+        
+ let datail= await searchDetail(id);
+
+
+
+        detalies.innerHTML=`
+          
+                
+                  <div class="cardPoster border border-5 rounded-5 w-75 ">
+        
+                      <div class="button-text d-flex justify-content-end  ">
+                            <button type="button" class="btn btn-primary buttonClose m-3 ">❌</button>
+                        </div>
+        
+                    <div class="d-flex justify-content-center">
+                         <h1 class="mt-5">Film info</h1>
+                            <hr>
+                    </div>
+        
+        
+               <div class="d-flex justify-content-center align-items-center ">
+             <img src="${datail.Poster}"
+                        class="card-img-top  imgPoster " alt="pic">
+                        
+                    <div class="card-body-poster d-flex flex-column justify-content-between">
+                       
+                        <div class="content-text">
+                           <div class="d-flex align-items-center"> <p class="card-title">Title:</p>
+                            <span> название</span></div>
+                           <div class="d-flex  align-items-center mt-5"> <p class="card-title">Released:</p>
+                            <span> название</span></div>
+                            <div class="d-flex  align-items-center mt-5"> <p class="card-title">Genre:</p>
+                             <span> название</span></div>
+                           <div class="d-flex  align-items-center mt-5"> <p class="card-title">Country:</p>
+                            <span> название</span></div>
+                           <div class="d-flex  align-items-center mt-5"> <p class="card-title">Director:</p>
+                            <span> название</span></div>
+                           <div class="d-flex  align-items-center mt-5"> <p class="card-title">Writer:</p>
+                            <span> название</span></div>
+                           <div class="d-flex  align-items-center mt-5"> <p class="card-title">Actors:</p>
+                            <span> название</span></div>
+                           <div class="d-flex  align-items-center mt-5"> <p class="card-title">Awards:</p>
+                            <span> название</span></div>
+                    
+                           
+                        </div>
+                       
+                    </div>
+        
+        
+        
+               </div>
+            </div>`;
+ 
+    }
+        })
+
+
+
+
+
 
 
 
