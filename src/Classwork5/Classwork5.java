067ab6f333a7e0c2955d605a2e629ee00b902b5b
Dmitrii <sorokindm1try@yandex.ru>
2025-11-27 11:57:44 +0300

    classwork5 1.0

---
+++ src/Classwork5/Classwork5.java
@@ -0,0 +1,16 @@
+package Classwork5;
+
+public class Classwork5 {
+    public static void main(String[] args) {
+
+//        try {
+//            Class.forName("com.mysql.cj.jdbc.Driver");
+//            System.out.println("Good");
+//        } catch (ClassNotFoundException e) {
+//            System.out.println("Error: " + e.getMessage());
+//        }
+
+
+
+    }
+}
